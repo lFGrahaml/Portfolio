fa53dbd6f8bbeaefa43656d31a87f9211cf1e831
lFGrahaml <Fgraham1@asu.edu>
2025-08-27 11:46:36 -0700

    008-Journal-TRPC: +tRPC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -18,10 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body className={`${poppins.className} text-2xl antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en" className="scroll-smooth">
+        <body className={`${poppins.className} text-2xl antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
+          {children}
+        </body>
+      </html>
+    </TRPCReactProvider>
+    
   );
 }
